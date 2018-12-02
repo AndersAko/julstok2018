c8e00f1733c8f7899cd22967824e3fd42f8987c7
Anders Kökeritz <anders.kokeritz@gmail.com>
2018-12-02 05:59:53 +0100

    Grundsetup

---
+++ Advent2018_common/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Advent2018_common
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string[] paket = System.IO.File.ReadAllLines("../../input.txt");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
