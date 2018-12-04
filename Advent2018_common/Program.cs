00d078a99c6a01e26e3c248cf44fe43ab4d77f81
Anders Kökeritz <anders.kokeritz@gmail.com>
2018-12-04 06:06:12 +0100

    Day 3

--- Advent2018_common/Program.cs
+++ Advent2018_common/Program.cs
@@ -4,92 +4,88 @@ using System.Linq;
 
 namespace Advent2018_common
 {
+    class Claim
+    {
+        public int id;
+        public int startx;
+        public int starty;
+        public int widthx;
+        public int heighty;
+
+        // Construct from string like: "#9 @ 593,72: 14x22"
+        public Claim(string input)
+        {
+            var coords_str = input.Split(new char[] { '#', ' ', '@', ':', 'x', ',' }, StringSplitOptions.RemoveEmptyEntries);
+            var coords = coords_str.Select(x => { int result;  var success = Int32.TryParse(x, out result); return success ? result : 0; }).ToArray();
+            id = coords[0];
+            startx = coords[1];
+            starty = coords[2];
+            widthx = coords[3];
+            heighty = coords[4];
+        }
+        public override string ToString()
+        {
+            return $"{id}: {startx},{starty}:{widthx}x{heighty}";
+        }
+        public Tuple<int, int>[] Overlaps(Claim two)
+        {
+            if (this.startx>= two.startx+two.widthx || this.starty >= two.starty+two.heighty ||
+                two.startx >= this.startx + this.widthx || two.starty >= this.starty + this.heighty)
+            {
+                return new Tuple<int, int>[0];
+            }
+            var result = new List<Tuple<int, int>>();
+            for (int x = this.startx; x<this.startx+this.widthx; x++)
+            {
+                for (int y=this.starty; y<this.starty+this.heighty; y++)
+                {
+                    if (x>=two.startx && x < two.startx + two.widthx && y>=two.starty & y<two.starty+two.heighty)
+                    {
+                        result.Add(new Tuple<int, int>(x, y));
+                    }
+                }
+            }
+            return result.ToArray();
+        }
+    }
+
     class MainClass
     {
-        // HashSet
-        private static Dictionary<char, int> letterCount(string input)
-        {
-            var result = new Dictionary<char, int>();
-            for (int i=0; i< input.Length; i++)
-            {
-                if (result.ContainsKey(input[i])) {
-                    result[input[i]] += 1;
-                }
-                else
-                {
-                    result[input[i]] = 1;
-                }
-            }
-            return result;
-        }
-
-        private static string diffMedEtt(string a, string[] lines)
-        {
-            // Jämför med alla andra rader, tecken för tecken
-            foreach (var b in lines)
-            {
-                int diffs = 0;
-                for (int i = 0; i < a.Length; i++)
-                {
-                    if (a[i] != b[i])
-                    {
-                        diffs++;
-                    }
-                }
-                if (diffs == 1)
-                {
-                    Console.WriteLine($"Hittade diff med ett mellan {a} och {b}");
-                    string result = "";
-                    for (int i = 0; i < a.Length; i++)
-                    {
-                        if (a[i] == b[i])
-                        {
-                            result += a[i];
-                        }
-                    }
-                    return result;
-                }
-            }
-            return "";
-        }
 
         public static void Main(string[] args)
         {
             string[] input = System.IO.File.ReadAllLines("../../input.txt");
-            int tvåor = 0;
-            int treor = 0;
-
+            var claims = new List<Claim>();
             foreach (var line in input)
             {
-                var bokstäver = letterCount(line);
-                bool settTvå = false;
-                bool settTre = false;
-                foreach (var b in bokstäver)
+                var c = new Claim(line);
+                claims.Add(c);
+            }
+
+            var overlaps = new HashSet<Tuple<int, int>>();
+            foreach (var claim in claims)
+            {
+                bool doesOverlap = false;
+                foreach (var claimTwo in claims)
                 {
-                    if (b.Value == 2 && !settTvå)
+                    if (claim != claimTwo)
                     {
-                        tvåor++;
-                        settTvå = true;
+                        //Console.Write($"{claim} and {claimTwo} overlaps at:");
+                        foreach (var overlap in claim.Overlaps(claimTwo))
+                        {
+                            //Console.Write($"{overlap}");
+                            overlaps.Add(overlap);
+                            doesOverlap = true;
+                        }
+                        //Console.WriteLine();
                     }
-                    if (b.Value == 3 && !settTre)
-                    {
-                        treor++;
-                        settTre = true;
-                    }
-
-
                 }
-                Console.WriteLine($"Counts: {line}: {tvåor}, {treor}");
+                if (!doesOverlap)
+                {
+                    Console.WriteLine($"Claim: {claim} has no overlap with any other.");
+                }
             }
-
-            Console.WriteLine($"Resultat: {tvåor} * {treor} = {tvåor * treor}");
-
-            foreach (var line in input)
-            {
-                var commonString = diffMedEtt(line, input);
-                if (commonString != "") Console.WriteLine(commonString);
-            }
-
+            Console.WriteLine($"Overlaps: {overlaps.Count()}");
         }
     }
 }
