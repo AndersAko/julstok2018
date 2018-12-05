53b83d8ff0f9e46bc62de2f212c77de518228a57
Anders Kökeritz <anders.kokeritz@gmail.com>
2018-12-05 06:37:42 +0100

    Day 5 initial

--- Advent2018_common/Program.cs
+++ Advent2018_common/Program.cs
@@ -1,72 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+
 
 namespace Advent2018_common
 {
 
     class MainClass
     {
+        private static StringBuilder reducePolymer (StringBuilder input)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i=0; i< input.Length-1; i++)
+            {
+                if (Char.IsLower(input[i]) && Char.IsUpper(input[i+1]) &&
+                 input[i] == char.ToLower(input[i+1]) ){
+
+                    i++; 
+                    continue;
+                }
+                if (Char.IsUpper(input[i]) && Char.IsLower(input[i+1]) &&
+                                 input[i] == char.ToUpper(input[i + 1]) ) {
+                    i++; 
+                    continue;
+                }
+                result.Append(input[i]);
+            }
+            result.Append(input[input.Length - 1]);
+            return result;
+        }
 
         public static void Main(string[] args)
         {
-            string[] input = System.IO.File.ReadAllLines("../../input.txt");
-            var sorted = (from line in input orderby line select line).ToArray();
-
-            // Array of 60 ints for each guard, one slot per minute. Number indicates number of occurances asleep
-            var guards = new Dictionary<int, int[]>();
-            int activeGuard = 0;
-            int asleepAtMinute = 0;
-            
-            for (int i=0; i<sorted.Count(); i++)
+            string[] input = System.IO.File.ReadAllLines("../../test.txt");
+            var polymer = new StringBuilder( input[0] );
+            while (true)
             {
-                // Parse 
-                //      [1518-02-08 23:57] Guard #1439 begins shift
-                //      [1518-02-09 00:16] falls asleep
-                //      [1518-02-09 00:53] wakes up
-                //      [1518-02-09 00:56] falls asleep
-                //      [1518-02-09 00:57] wakes up
-                var splitLine = sorted[i].Split(" :][#".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
-                if (splitLine[3] == "Guard")
-                {
-                    activeGuard = Convert.ToInt32(splitLine[4]);
-                    if (!guards.ContainsKey(activeGuard))
-                    {
-                        guards[activeGuard] = new int[60];
-                    }
-                }
-                else if (splitLine[3] == "falls")
-                {
-                    asleepAtMinute = Convert.ToInt32(splitLine[2]);
-                } else if (splitLine[3] == "wakes")
-                {
-                    var toMinute = Convert.ToInt32(splitLine[2]);
-
-                    for (int j = asleepAtMinute; j < toMinute; j++)
-                    {
-                        guards[activeGuard][j]++;
-                    }
-                }
-                string text = String.Join("", guards[activeGuard] );
-                Console.WriteLine($"{activeGuard}: {sorted[i]}: {text}");
+                var reduced = reducePolymer(polymer);
+                if (reduced.Length == polymer.Length) break;
+                Console.WriteLine($"{reduced.Length}: {reduced}");
+                polymer = reduced;
             }
-            foreach (var guard in guards)
-            {
-                string text = String.Join("", guard.Value);
-                Console.WriteLine($"{guard.Key}: {text}");
-            }
-            var asleepMinutes = (from guard in guards select (guard.Key, guard.Value.Sum())); 
-            var maxAsleepGuard = (from guard in asleepMinutes where guard.Item2 == asleepMinutes.Max(x => x.Item2) select guard).First();
-            Console.WriteLine($"Most asleep guard: { maxAsleepGuard.Item1}, sleeping { maxAsleepGuard.Item2} ");
-            Console.WriteLine($"Most asleep minute: {Array.IndexOf(guards[maxAsleepGuard.Item1], guards[maxAsleepGuard.Item1].Max())}");
-            Console.WriteLine($"Result: {maxAsleepGuard.Item1 * Array.IndexOf(guards[maxAsleepGuard.Item1], guards[maxAsleepGuard.Item1].Max())}");
-
-            var mostFrequentAsleep = (from guard in guards select (guard.Key, guard.Value.Max()));
-            var mostFrequentAsleepGuard = (from guard in mostFrequentAsleep where guard.Item2 == mostFrequentAsleep.Max(x => x.Item2) select guard).First();
-            Console.WriteLine($"Most frequently asleep guard: { mostFrequentAsleepGuard.Item1}, sleeping { mostFrequentAsleepGuard.Item2} times");
-            Console.WriteLine($"Most asleep minute: {Array.IndexOf(guards[mostFrequentAsleepGuard.Item1], guards[mostFrequentAsleepGuard.Item1].Max())}");
-            Console.WriteLine($"Result: {mostFrequentAsleepGuard.Item1 * Array.IndexOf(guards[mostFrequentAsleepGuard.Item1], guards[mostFrequentAsleepGuard.Item1].Max())}");
-
+            Console.WriteLine($"{polymer.Length}: {polymer}");
         }
     }
 }
