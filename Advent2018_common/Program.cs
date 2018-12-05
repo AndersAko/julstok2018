5e0b820500031c60c2a65b21a99342007e015f4c
Anders Kökeritz <anders.kokeritz@gmail.com>
2018-12-05 07:11:26 +0100

    Day 5 complete (unoptimized)

--- Advent2018_common/Program.cs
+++ Advent2018_common/Program.cs
@@ -11,38 +11,49 @@ namespace Advent2018_common
     {
         private static StringBuilder reducePolymer (StringBuilder input)
         {
-            StringBuilder result = new StringBuilder();
-            for (int i=0; i< input.Length-1; i++)
+            StringBuilder result = input;
+            for (int i=0; i< result.Length-1; i++)
             {
-                if (Char.IsLower(input[i]) && Char.IsUpper(input[i+1]) &&
-                 input[i] == char.ToLower(input[i+1]) ){
-
-                    i++; 
+                if (Char.IsLower(result[i]) && Char.IsUpper(result[i+1]) &&
+                 result[i] == char.ToLower(result[i+1]) ){
+                    result = result.Remove(i, 2);
                     continue;
                 }
-                if (Char.IsUpper(input[i]) && Char.IsLower(input[i+1]) &&
-                                 input[i] == char.ToUpper(input[i + 1]) ) {
-                    i++; 
-                    continue;
+                if (Char.IsUpper(result[i]) && Char.IsLower(result[i+1]) &&
+                                 result[i] == char.ToUpper(result[i + 1]) ) {
+                    result = result.Remove(i, 2);
                 }
-                result.Append(input[i]);
             }
-            result.Append(input[input.Length - 1]);
             return result;
         }
 
         public static void Main(string[] args)
         {
-            string[] input = System.IO.File.ReadAllLines("../../test.txt");
+            string[] input = System.IO.File.ReadAllLines("../../input.txt");
             var polymer = new StringBuilder( input[0] );
-            while (true)
+
+            int minimumPolymerLength = Int32.MaxValue;
+
+            string units = "abcdefghijklmnopqrstuvwxyz";
+            foreach (var unit in units.ToCharArray())
             {
-                var reduced = reducePolymer(polymer);
-                if (reduced.Length == polymer.Length) break;
-                Console.WriteLine($"{reduced.Length}: {reduced}");
-                polymer = reduced;
+                polymer = new StringBuilder(input[0]);
+                polymer = polymer.Replace(Char.ToLower(unit).ToString() , "");
+                polymer = polymer.Replace(Char.ToUpper(unit).ToString(), "");
+
+                var length = polymer.Length;
+                while (true)
+                {
+                    var reduced = reducePolymer(polymer);
+                    if (reduced.Length == length) break;
+                    polymer = reduced;
+                    length = polymer.Length;
+                }
+                if (polymer.Length < minimumPolymerLength) minimumPolymerLength = length;
+                Console.WriteLine($"{polymer.Length}: {polymer}");
+
             }
-            Console.WriteLine($"{polymer.Length}: {polymer}");
+            Console.WriteLine($"{minimumPolymerLength}");
         }
     }
 }
