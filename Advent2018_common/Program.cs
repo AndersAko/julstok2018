1f0845d3dc5d65ef7d63565bfbe4411813c9cca4
Anders Kökeritz <anders.kokeritz@gmail.com>
2018-12-06 22:14:07 +0100

    Day 6

--- Advent2018_common/Program.cs
+++ Advent2018_common/Program.cs
@@ -14,25 +14,27 @@ namespace Advent2018_common
     {
         public int id;
         public Point pos;
-        public List<Point> closest;
-        public bool infinite=true;
+        public HashSet<Point> closest;
+        public bool infinite = false;
+        public bool complete = false;
 
-        internal List<Point> GetSquaresAtDistance(int distance, Point min, Point max)
+        internal static void Add(HashSet<Point> set, Point p, Point min, Point max, HashSet<Point> visited)
         {
-            var squares = new List<Point>();
+            if (p.x < min.x || p.x > max.x) return;
+            if (p.y < min.y || p.y > max.y) return;
+            if (visited.Contains(p)) return;
+            set.Add(p);
+        }
+        internal HashSet<Point> GetSquaresAtDistance(int distance, Point min, Point max, HashSet<Point> visited)
+        {
+            var squares = new HashSet<Point>();
 
             for (int x = -distance; x <= distance; x++)
             {
                 var y = distance - Math.Abs(x);
-                if (!(pos.x + x > min.x && pos.x + x < max.x)) break;
-                if (pos.y + y > min.y && pos.y + y < max.y)
-                {
-                    squares.Add(new Point(pos.x + x, pos.y + y));
-                }
-                if (pos.y - y > min.y && pos.y - y < max.y && y!= 0)
-                {
-                    squares.Add(new Point(pos.x + x, pos.y - y));
-                }
+                Coord.Add(squares, new Point(pos.x + x, pos.y + y), min, max, visited);
+                Coord.Add(squares, new Point(pos.x + x, pos.y - y), min, max, visited);
+
             }
             return squares;
         }
@@ -48,11 +50,16 @@ namespace Advent2018_common
         {
             return $"{x},{y}";
         }
-        
-    }
-    public static class Extensions
-    {
-        public static string ToString(this List<Point> points) => "[ " + String.Join(",", points.Select(x => x.ToString()).ToArray()) + "]";
+        public int DistanceTo(Point p) => Math.Abs(p.x - x) + Math.Abs(p.y - y);
+        internal int DistanceTo(List<Coord> coords)
+        {
+            int sum = 0;
+            foreach (var c in coords)
+            {
+                sum += DistanceTo(c.pos);
+            }
+            return sum;
+        }
     }
 
     class MainClass
@@ -66,19 +73,20 @@ namespace Advent2018_common
 
             Point max = new Point();
             Point min = new Point();
-            
-            for (int i=0; i< input.Length; i++)
+
+            for (int i = 0; i < input.Length; i++)
             {
                 var x = new Coord();
                 var inputCoords = input[i].Split(", ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).Select(c => Convert.ToInt32(c)).ToArray();
                 x.pos = new Point(inputCoords[0], inputCoords[1]);
-                x.closest = new List<Point>();
+                x.closest = new HashSet<Point>();
 
                 if (i == 0)
                 {
                     max = x.pos;
                     min = x.pos;
-                } else
+                }
+                else
                 {
                     if (x.pos.x > max.x) { max.x = x.pos.x; }
                     if (x.pos.y > max.y) { max.y = x.pos.y; }
@@ -90,72 +98,86 @@ namespace Advent2018_common
                 coords.Add(x);
             }
             Console.WriteLine($"Extents: {min} to {max}");
-            foreach(var c in coords)
+            foreach (var c in coords)
             {
                 Console.WriteLine($"{c.id}: {c.pos.x}, {c.pos.y}");
             }
             // Try increasing manhattan distances upto a reasonable maximum
-            var visitedSquares = new List<Point>();
-            
-            for (int distance=0; distance < max.x-min.x && distance<max.y-min.y; distance++)
+            var visitedSquares = new HashSet<Point>();
+
+            for (int distance = 0; distance < max.x - min.x && distance < max.y - min.y; distance++)
             {
                 Console.WriteLine($"DISTANCE: {distance}");
-                var visitedSquaresThisDistance = new List<Point>();
+                var visitedSquaresThisDistance = new HashSet<Point>();
+                var searchComplete = true;
                 foreach (var c in coords)
                 {
-                    var squaresAtDistance = c.GetSquaresAtDistance(distance, min, max);
-                    
-                    // Remove squares that are already visited ie closer to some other point
-                    squaresAtDistance = squaresAtDistance.Except(visitedSquares).ToList();
-                   
-                    c.closest = c.closest.Concat(squaresAtDistance).ToList();
+                    if (c.complete) continue;
+                    searchComplete = false;
+                    var squaresAtDistance = c.GetSquaresAtDistance(distance, min, max, visitedSquares);
 
-                    List<Point> squaresAtEqualDistance = squaresAtDistance.Intersect(visitedSquaresThisDistance).ToList(); 
+                    c.closest.UnionWith(squaresAtDistance);
+
+                    HashSet<Point> squaresAtEqualDistance = new HashSet<Point>(squaresAtDistance.Intersect(visitedSquaresThisDistance));
                     if (squaresAtEqualDistance.Count != 0)
                     {
-                        // Console.WriteLine($"Some squares are equal distance to two points: {String.Join("; ", squaresAtEqualDistance.Select(x => x.ToString()).ToArray())}");
-                        // Remove duplicate squares from all coords
                         foreach (var c2 in coords)
                         {
-                            c2.closest = c2.closest.Except(squaresAtEqualDistance).ToList();
+                            c2.closest.ExceptWith(squaresAtEqualDistance);
                         }
                     }
-                    if (squaresAtDistance.Count - squaresAtEqualDistance.Count == 0)
-                    {
-                        c.infinite = false;
-                    }
-                    visitedSquaresThisDistance = visitedSquaresThisDistance.Union(squaresAtDistance).ToList();
+                    c.infinite = c.closest.Any(p => p.x == min.x || p.y == min.y || p.x == max.x || p.y == max.y);
+
+                    if (squaresAtDistance.Count - squaresAtEqualDistance.Count == 0) c.complete = true;
+                    visitedSquaresThisDistance.UnionWith(squaresAtDistance);
                 }
-                visitedSquares = visitedSquares.Concat(visitedSquaresThisDistance).ToList();
+                visitedSquares.UnionWith(visitedSquaresThisDistance);
                 foreach (var c in coords)
                 {
-                    Console.WriteLine($"{c.id}: {c.pos.x}, {c.pos.y} Area: {c.closest.Count} {c.infinite}");
+                    Console.WriteLine($"{c.id}: {c.pos.x}, {c.pos.y} Area: {c.closest.Count} {c.infinite} {c.complete}");
                 }
+                var largestArea = coords.Where(c => !c.infinite).OrderByDescending(c => c.closest.Count).First();
+                Console.WriteLine($"Greatest of them all: {largestArea.id} at {largestArea.pos.x}, {largestArea.pos.y} with size {largestArea.closest.Count}");
+                if (searchComplete) break;
             }
             Console.WriteLine($"x: {min.x} to {max.x}");
-            for (int y=min.y-5; y<=max.y+5; y++)
+            for (int y = min.y - 5; y <= max.y + 5; y++)
             {
                 Console.Write($"{y,3}: ");
-                for (int x = min.x-5; x <= max.x+5; x++)
+                for (int x = min.x - 5; x <= max.x + 5; x++)
                 {
-                    var coordClosestToThisPoint = coords.Where(c => c.closest.Contains(new Point(x,y)));
+                    var coordClosestToThisPoint = coords.Where(c => c.closest.Contains(new Point(x, y)));
                     var coordAtThisPoint = coords.Where(c => c.pos.x == x && c.pos.y == y);
                     if (coordAtThisPoint.Count() == 1)
                     {
-                        Console.Write((char) (coordAtThisPoint.First().id + 'a') + " ");
-                    } else if (coordClosestToThisPoint.Count() == 1)
+                        Console.Write((char)(coordAtThisPoint.First().id % 26 + 'A'));
+                    }
+                    else if (coordClosestToThisPoint.Count() == 1)
                     {
-                        Console.Write((char) (coordClosestToThisPoint.First().id + 'a')+" ");
-                    } else if (coordClosestToThisPoint.Count() > 1)
+                        Console.Write((char)(coordClosestToThisPoint.First().id % 26 + 'a'));
+                    }
+                    else if (coordClosestToThisPoint.Count() > 1)
                     {
-                        Console.Write(coordClosestToThisPoint.Count()+" ");
-                    } else
+                        Console.Write(coordClosestToThisPoint.Count());
+                    }
+                    else
                     {
-                        Console.Write(". ");
+                        Console.Write(".");
                     }
                 }
                 Console.WriteLine();
             }
+
+            // Part Two
+            int lessThan10000 = 0;
+            for (int y = min.y - 5; y <= max.y + 5; y++)
+            {
+                for (int x = min.x - 5; x <= max.x + 5; x++)
+                {
+                    if (new Point(x, y).DistanceTo(coords) < 10000) lessThan10000++;
+                }
+            }
+            Console.WriteLine($"Size of region closest to all: {lessThan10000}");
             Console.ReadKey();
         }
     }
