b150617745ab2956908bd24af93b3940ece3820e
Anders Kökeritz <anders.kokeritz@gmail.com>
2018-12-08 09:29:33 +0100

    Day 8

--- Advent2018_common/Program.cs
+++ Advent2018_common/Program.cs
@@ -6,22 +6,70 @@ using System.Text;
 
 namespace Advent2018_common
 {
-    internal class Instruction
+
+    public class Node
     {
-        public string id;
-        public List<Instruction> nextSteps;
-        public List<Instruction> previousSteps;
-        public bool completed = false;
-        public Nullable<int> endAtSecond;
+        public List<Node> childNodes { get; set; } = new List<Node>() ;
+        public List<int> metaData { get; set; } = new List<int>() ;
+        int id;
 
-        public Instruction(string id)
+        static int nextId = 0;
+
+        public Node(IEnumerator<int> inputSequence)
         {
-            this.id = id;
-            this.nextSteps = new List<Instruction>();
-            this.previousSteps = new List<Instruction>();
-        }
-    }
+            id = nextId++;
 
+            var numChildren = inputSequence.Current;
+            if (!inputSequence.MoveNext()) Console.WriteLine("Failed to parse!");
+
+            var numMetadata = inputSequence.Current;
+            if (!inputSequence.MoveNext()) Console.WriteLine("Failed to parse!!");
+
+            for (int i=0; i<numChildren; i++)
+            {
+                childNodes.Add(new Node(inputSequence));
+            }
+            for (int i = 0; i < numMetadata; i++)
+            {
+                var meta = inputSequence.Current;
+                if (!inputSequence.MoveNext()) Console.WriteLine("Failed to parse metadata");
+
+                metaData.Add(meta);
+            }
+        }
+        public int SumMetadata()
+        {
+            return metaData.Sum() + childNodes.Select(c => c.SumMetadata()).Sum();
+        }
+        public int Value()
+        {
+            if (childNodes.Count() == 0) return metaData.Sum();
+
+            int value = 0; 
+            foreach (var index in metaData)
+            {
+                 value += (childNodes.ElementAtOrDefault(index-1)?.Value() ?? 0);
+            }
+            return value;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder($"{id}: C(");
+            foreach (var c in childNodes)
+            {
+                result.Append($"{c.id} ");
+            }
+            result.Append (") M( ");
+            foreach (var m in metaData)
+            {
+                result.Append($"{m} ");
+            }
+            result.Append(")");
+            return result.ToString();
+        }
+
+    }
     class MainClass
     {
 
@@ -29,94 +77,17 @@ namespace Advent2018_common
         {
             string[] input = System.IO.File.ReadAllLines("../../input.txt");
 
-            Dictionary<string, Instruction> instructions = new Dictionary<string, Instruction>();
+            var inputSequence = input[0].Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).Select(x => Convert.ToInt32(x)).GetEnumerator();
 
-            // Parse
-            foreach (var line in input)
-            {
-                var splitLine = line.Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).ToArray();
-                var stepBeforeId = splitLine[1];
-                var stepAfterId = splitLine[7];
+            inputSequence.MoveNext();
+            var root = new Node(inputSequence);
 
-                // Find after instruction if it already exists, otherwise create it
-                Instruction stepAfter;
-                if (!instructions.ContainsKey(stepAfterId))
-                {
-                    stepAfter = new Instruction(stepAfterId);
-                    instructions[stepAfterId] = stepAfter;
-                }
-                stepAfter = instructions[stepAfterId];
+            Console.WriteLine($"Root node: {root}.");
+            foreach (var c in root.childNodes) Console.WriteLine($"Children: {c}.");
 
-                // Find before instruction if it already exists, otherwise create it
-                Instruction stepBefore;
-                if (!instructions.ContainsKey(stepBeforeId))
-                {
-                    stepBefore = new Instruction(stepBeforeId);
-                    instructions[stepBeforeId] = stepBefore;
-                }
-                stepBefore = instructions[stepBeforeId];
+            Console.WriteLine($"Sum of all metadata: {root.SumMetadata()}");
+            Console.WriteLine($"Value of root: {root.Value()}");
 
-                stepBefore.nextSteps.Add(stepAfter);
-                stepAfter.previousSteps.Add(stepBefore);
-            }
-            var first = instructions.Where(s => s.Value.previousSteps.Count == 0).First();
-            Console.Write($"Found {instructions.Where(s => s.Value.previousSteps.Count == 0).Count()} first steps: ");
-            foreach (var i in instructions.Where(s => s.Value.previousSteps.Count == 0))
-            {
-                Console.Write($"{i.Value.id} ");
-            }
-            Console.WriteLine();
-
-            List <Instruction> available = new List<Instruction>( instructions.Where(s => s.Value.previousSteps.Count == 0).Select(i => i.Value) );
-
-            StringBuilder sequence = new StringBuilder("");
-            while (available.Count > 0)
-            {
-                var nextAvailable = available.Where(i => i.previousSteps.Count == 0 || i.previousSteps.All(pi => pi.completed)).OrderBy(i => i.id).First();
-            
-                sequence.Append(nextAvailable.id);
-                nextAvailable.completed = true;
-                available.Remove(nextAvailable);
-                available = available.Union(nextAvailable.nextSteps).ToList();
-
-            }
-            Console.WriteLine($" Correct sequence: {sequence}");
-
-            // Part 2
-            // Reset complete flag
-            foreach (var i in instructions) i.Value.completed = false;
-            sequence = new StringBuilder("");
-
-            int secondNow = 0;
-            var workInProgress = new List<Instruction>();
-
-            while (true)
-            {
-                available = new List<Instruction>(instructions
-                .Where(s => (s.Value.previousSteps.Count == 0 || s.Value.previousSteps.All(pi => pi.completed)) && !s.Value.endAtSecond.HasValue)
-                .Select(i => i.Value)
-                .OrderBy(i => i.id));
-                if (available.Count == 0 && workInProgress.Count == 0) break;
-
-                // Add more workers if needed
-                if (workInProgress.Count < 6 && available.Count > 0)
-                {
-                    var nextAvailable = available.First();
-                    nextAvailable.endAtSecond = secondNow + 60 + nextAvailable.id.ToCharArray()[0] - 'A' + 1 ;
-                    workInProgress.Add(nextAvailable);
-                    continue;
-                }
-
-                // Complete new completed work...
-                var nextToComplete = workInProgress.OrderBy(i => i.endAtSecond).First();
-                sequence.Append(nextToComplete.id);
-                secondNow = nextToComplete.endAtSecond.Value;
-                nextToComplete.completed = true;
-                workInProgress.Remove(nextToComplete);
-
-                // Console.WriteLine($" {secondNow}: sequence: {sequence} workers: {string.Join(" ", workInProgress.Select(i => $"{i.id} {i.endAtSecond}"))}");
-            }
-            Console.WriteLine($"Work completed in {secondNow} seconds.");
             Console.ReadKey();
         }
     }
