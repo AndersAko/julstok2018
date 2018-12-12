a0027b82a1431ee7ff6f3a2e3597daee8d6c5084
Anders Kökeritz <Anders.Kokeritz@designevolution.se>
2018-12-12 18:14:51 +0100

    Day 12 continued

--- Advent2018_common/Program.cs
+++ Advent2018_common/Program.cs
@@ -9,23 +9,64 @@ namespace Advent2018_common
 {
     class Pots
     {
-        HashSet<int> state = new HashSet<int>();
+        BitArray state=new BitArray(3, false);
+        public int StartIndex = -3;
 
         public Pots (string initialState)
         {
-            int i = 0;
-            foreach (var c in initialSstate)
+            foreach (var c in initialState)
             {
-                if (c == "#") state.Add(i);
-                i++;
+                state.Length++;
+                state[state.Length - 1] = (c == '#');
             }
+            state.Length += 3;
         }
-    }
-    class Rule
-    {
-        BitArray pattern;
-        bool nextGenPlant;
+        public Pots (BitArray b, int i)
+        {
+            state = b;
+            StartIndex = i;
+        }
+        public Pots NextGeneration(List<BitArray> patterns)
+        {
+            var nextState = new BitArray(state.Length, false);
+            var nextStartIndex = StartIndex;
+           
 
+            for (int i=0; i<state.Length-5; i++)
+            {
+                foreach (var p in patterns)
+                {
+                    var match = true;
+                    for (int b=0; b<5; b++)
+                    {
+                        if (state[i+b] != p[b]) 
+                        {
+                            match = false;
+                            break;
+                        }
+                    }
+                    if (match)  // Found a pattern that indicates a new plant at index i+2 (middle of pattern) = pot number i+2+StartIndex
+                    {
+                        // New plant at first position (ie at index 2), add a new blank cell before to keep three blanks 
+                        if (i==0)
+                        {
+                            nextStartIndex--;
+                        }
+                        nextState[i + 2 + StartIndex - nextStartIndex] = true;
+                    } 
+                }
+            }
+            return new Pots(nextState, nextStartIndex);
+        }
+        public override string ToString()
+        {
+            var result = new StringBuilder($"{StartIndex}");
+            for (int i=0; i<state.Length; i++)
+            {
+                result.Append(state[i] ? "#" : ".");
+            }
+            return result.ToString();
+        }
     }
 
     class MainClass
@@ -36,7 +77,22 @@ namespace Advent2018_common
             string[] input = System.IO.File.ReadAllLines("../../test.txt");
             Pots pots = new Pots(input[0].Split(" :".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)[2]);
 
-
+            var patterns = new List<BitArray>();
+            for (int i=2; i<input.Length; i++)
+            {
+                if (input[i].Split(" ".ToCharArray())[2]=="#")
+                {
+                    var inputPattern = input[i].Split(" ".ToCharArray())[0].ToCharArray().Select(x => x == '#');
+                    var pattern = new BitArray(inputPattern.ToArray());
+                    patterns.Add(pattern);
+                }
+            }
+            
+            for (var generation = 1; generation<=20; generation++)
+            {
+                pots = pots.NextGeneration(patterns);
+                Console.WriteLine($"{generation,3}:{new String(' ',7+pots.StartIndex)}{pots}");
+            }
             Console.WriteLine($"Maxmimum sum");
             Console.ReadKey();
         }
