e1c2498755f7bca5d6d9f3b6550b2c1fe8aa6745
Anders Kökeritz <anders.kokeritz@gmail.com>
2018-12-14 07:51:33 +0100

    Day 14 star 1

--- Advent2018_common/Program.cs
+++ Advent2018_common/Program.cs
@@ -10,138 +10,114 @@ namespace Advent2018_common
 
     class MainClass
     {
-        class Cart
+        public static string Puzzle(int numberOfRecipesBefore)
         {
-            public (int X, int Y) Pos;
-            public int Dir { get; set; }    // Up = 0, left = 1, ..
-            internal int nextTurn;                   // 0 = left, 1 = straight, 2 = right
+            var recipes = new List<int> { 3, 7 };
+            int elfOne = 0;
+            int elfTwo = 1;
 
-            public Cart(int row, int col, int dir)
+            for (int i = 0; i < numberOfRecipesBefore + 10; i++)
             {
-                Pos.X = col;
-                Pos.Y = row;
-                Dir = dir;
-                nextTurn = 0;
+                // Create new recipies
+                var newMix = recipes[elfOne] + recipes[elfTwo];
+                var newRecipe = newMix / 10;
+                if (newRecipe != 0) recipes.Add(newRecipe);
+
+                newRecipe = newMix % 10;
+                recipes.Add(newRecipe);
+
+                // Move
+                elfOne = (elfOne + 1 + recipes[elfOne]) % recipes.Count;
+
+                elfTwo = (elfTwo + 1 + recipes[elfTwo]) % recipes.Count;
+
+                if (i<20)
+                {
+                    Console.Write($"{i} - {elfOne};{elfTwo}: ");
+                    foreach (var r in recipes)
+                    {
+                        Console.Write($"{r} ");
+                    }
+                    Console.WriteLine();
+                }
             }
-            public char Symbol()
+            var result = new StringBuilder();
+            for (int i=numberOfRecipesBefore; i<numberOfRecipesBefore+10; i++)
             {
-                return "^<v>".ToCharArray()[Dir];
+                result.Append($"{recipes[i]}");
             }
-            public void NextPos(string[] tracks)
+            return result.ToString();
+        }
+
+        public static int PuzzlePattern(string pattern)
+        {
+            var recipes = new List<int> { 3, 7 };
+            int elfOne = 0;
+            int elfTwo = 1;
+
+            var recipeString = new StringBuilder("37");
+
+            for (int i=0; ; i++)
             {
-                switch (Dir)
+                // Create new recipies
+                var newMix = recipes[elfOne] + recipes[elfTwo];
+                var newRecipe = newMix / 10;
+                if (newRecipe != 0)
                 {
-                    case 0:
-                        Pos.Y--; break;
-                    case 1:
-                        Pos.X--; break;
-                    case 2:
-                        Pos.Y++; break;
-                    case 3:
-                        Pos.X++; break;
+                    recipes.Add(newRecipe);
+                    recipeString.Append($"{newRecipe}");
                 }
-                if (tracks[Pos.Y][Pos.X] == '+')
+
+                newRecipe = newMix % 10;
+                recipes.Add(newRecipe);
+                recipeString.Append($"{newRecipe}");
+
+                // Move
+                elfOne = (elfOne + 1 + recipes[elfOne]) % recipes.Count;
+
+                elfTwo = (elfTwo + 1 + recipes[elfTwo]) % recipes.Count;
+
+                if (i < 20)
                 {
-                    Dir = (Dir + 5 - nextTurn) % 4;
-                    nextTurn = (nextTurn + 1) % 3;
+                    Console.Write($"{i} - {elfOne};{elfTwo}: ");
+                    foreach (var r in recipes)
+                    {
+                        Console.Write($"{r} ");
+                    }
+                    Console.WriteLine(recipeString);
                 }
-                else if (tracks[Pos.Y][Pos.X] == '/')
+                
+                if (recipeString.Length >=5)
                 {
-                    Dir = (3 - Dir) % 4;
-                }
-                else if (tracks[Pos.Y][Pos.X] == '\\')
-                {
-                    Dir = (5 - Dir) % 4;
+                    char[] end = new char[pattern.Length];
+
+                    recipeString.CopyTo(recipeString.Length - pattern.Length, end, 0, pattern.Length);
+                    var endString = new string (end);
+
+                    if (pattern.Equals(new string(end) ))
+                    {
+                        Console.WriteLine("Pattern {pattern} is matching at {i}: {recipeString}");
+                        return recipeString.Length - pattern.Length;
+                    }
                 }
             }
         }
 
         public static void Main(string[] args)
         {
-            string[] input = System.IO.File.ReadAllLines("../../testPart2.txt");
+            //    string[] input = System.IO.File.ReadAllLines("../../testPart2.txt");
 
-            string[] tracks = new string[input.Length];    // Same format as input, but without carts
-            var carts = new List<Cart>();
+            Console.WriteLine($"Scores after 9: {Puzzle(9)}");
+            Console.WriteLine($"Scores after 5: {Puzzle(5)}");
+            Console.WriteLine($"Scores after 18: {Puzzle(18)}");
+            Console.WriteLine($"Scores after 074501: {Puzzle(074501)}");
 
-            for (int row = 0; row < input.Length; row++)
-            {
-                try
-                {
-                    int index = 0;
-                    while (true)
-                    {
-                        var cartIndex = input[row].IndexOfAny("^<v>".ToCharArray(), index);
-                        if (cartIndex == -1) break;
-                        index = cartIndex + 1;
-                        var dir = 0;
-                        switch (input[row][cartIndex])
-                        {
-                            case '^':
-                                dir = 0;
-                                break;
-                            case '<':
-                                dir = 1;
-                                break;
-                            case 'v':
-                                dir = 2;
-                                break;
-                            case '>':
-                                dir = 3;
-                                break;
-                        }
-                        carts.Add(new Cart(row, cartIndex, dir));
-                    }
-                }
-                catch (ArgumentNullException) { };
-                var line = input[row].Replace('^', '|').Replace('v', '|').Replace('<', '-').Replace('>', '-');
-                tracks[row] = line;
-            }
+            Console.WriteLine(PuzzlePattern("51589"));
+            Console.WriteLine(PuzzlePattern("01245"));
+            Console.WriteLine(PuzzlePattern("92510"));
+            Console.WriteLine(PuzzlePattern("59414"));
+            Console.WriteLine(PuzzlePattern("074501"));
 
-            Console.WriteLine("Initial position");
-            for (int row = 0; row < tracks.Length; row++)
-            {
-                var line = tracks[row].ToCharArray();
-                foreach (var cart in carts.Where(c => c.Pos.Y == row))
-                {
-                    line[cart.Pos.X] = cart.Symbol();
-                }
-                Console.WriteLine(new String(line));
-            }
-
-            while (carts.Count > 1)
-            {
-                foreach (var cart in carts.OrderBy(c => c.Pos.Y).OrderBy(c => c.Pos.X))
-                {
-                    cart.NextPos(tracks);
-
-                    var collisionAt = carts.GroupBy(c => c.Pos).Where(g => g.Count() > 1);
-
-                    if (collisionAt.Any())
-                    {
-                        for (int row = 0; row < tracks.Length; row++)
-                        {
-                            var line = tracks[row].ToCharArray();
-                            var cartText = new StringBuilder();
-                            foreach (var c in carts.Where(c => c.Pos.Y == row).OrderBy(c => c.Pos.X))
-                            {
-                                line[c.Pos.X] = c.Symbol();
-                                cartText.Append($"({c.Pos.X},{c.Pos.Y}): {c.Symbol()} {c.nextTurn}   ");
-                            }
-                            Console.WriteLine(new String(line) + cartText.ToString());
-                        }
-
-                        var crashSite = collisionAt.First().Key;
-
-                        Console.WriteLine($"Bang! at {crashSite.X}, {crashSite.Y}");
-                        foreach (var c in collisionAt.First())
-                        {
-                            carts.Remove(c);
-                        }
-                        Console.WriteLine($"{carts.Count} carts remaining");
-                    }
-                }  
-            }
-            Console.WriteLine($"One cart remaining at {carts.First().Pos.X}, {carts.First().Pos.Y}");
 
             Console.ReadKey();
         }
