67c771750a92d3ff62ffab3497c6f59a01a42bd2
Anders Kökeritz <Anders.Kokeritz@designevolution.se>
2018-12-21 16:30:00 +0100

    Day 16 star 1

--- Advent2018_common/Program.cs
+++ Advent2018_common/Program.cs
@@ -7,244 +7,145 @@ using System.Text;
 
 namespace Advent2018_common
 {
-    abstract class Unit
+    struct Instruction
     {
-        struct Route
+        string opCode;
+        int inputA;
+        int inputB;
+        int output;
+
+        public Instruction(string Opcode, int A, int B, int Out)
         {
-            internal (int X, int Y) endPos;
-            internal List<(int X, int Y)> path;
-            internal Route((int X, int Y) endPos, List<(int X, int Y)> path)
-            {
-                this.endPos = endPos;
-                this.path = new List<(int X, int Y)>(path);
-                this.path.Add(endPos);
-            }
-            internal Route((int X, int Y) endPos) : this(endPos, new List<(int X, int Y)>()) { }
-            public override string ToString()
-            {
-                return $"{path.Count}:{endPos.Y}.{endPos.X}";
-            }
+            opCode = Opcode;
+            inputA = A;
+            inputB = B;
+            output = Out;
         }
-        public (int X, int Y) Pos;
-        public int hp;
-        public Unit(int X, int Y)
-        {
-            hp = 200;
-            this.Pos = (X, Y);
+        public Instruction (string instruction) :this(instruction.Split(' ')[0], Convert.ToInt32(instruction.Split(' ')[1]), 
+            Convert.ToInt32(instruction.Split(' ')[2]), Convert.ToInt32(instruction.Split(' ')[3]))
+        {        
         }
-        public Unit (Unit unitToCopy)
-        {
-            Pos = unitToCopy.Pos;
-            hp = 200;
-        }
-        public abstract char Symbol();
-        // Move this unit to the closest reachable unit and return list of potential melee targets
-        public List<Unit> MoveAndFindTargets(List<string> maze, List<Unit> units)
-        {
-            // Already in range, no need to move
-            var meleeUnits = FindMeleeUnits(units);
-            if (meleeUnits.Any()) return meleeUnits;
 
-            // Otherwise, look for closest path to closest target
-            // Console.WriteLine($"Finding path for {this}");
-            var movesToSearch = new SortedList<string, Route>();
-            foreach (var newMove in PossibleMoves(Pos, maze, units).Select(m => new Route(m))) {
-                movesToSearch[newMove.ToString()] = newMove;
-            }
-            var examinedRoutes = new HashSet<(int X, int Y)>();
+        public int[] Execute(int[] inputRegisters)
+        {
+            //var result = new Dictionary<int, int>(inputRegisters);
+            var result = inputRegisters;
 
-            while (movesToSearch.Any())
+            switch (opCode)
             {
-                // Pick next position in list
-                var checkPosition = movesToSearch.First();
-                movesToSearch.RemoveAt(0);
-                examinedRoutes.Add(checkPosition.Value.endPos);
-
-                //Console.WriteLine($"Checking {checkPosition.Value}");
-
-                // If Adjacent to another unit, return a list of all melee units
-                var closestUnits = units.Where(u => u.GetType() != this.GetType())
-                                        .Where(u => new List<(int X, int Y)> { (0, -1), (-1, 0), (1, 0), (0, 1) }
-                                                        .Any(delta => u.Pos.X == checkPosition.Value.endPos.X + delta.X && u.Pos.Y == checkPosition.Value.endPos.Y + delta.Y));
-                if (closestUnits.Any())
-                {
-                    Pos = checkPosition.Value.path[0];    // First step
-                    return closestUnits.ToList();
-                }
-                // Find possible moves from here and add to list
-                foreach (var newMove in PossibleMoves(checkPosition.Value.endPos, maze, units)
-                                       .Where(newMove => !examinedRoutes.Contains(newMove))
-                                       .Select(m => new Route(m, checkPosition.Value.path)))
-                {
-                    movesToSearch[newMove.ToString()] = newMove;
-                }
-            }
-            //Console.WriteLine($"No units are reachable from {this}");
-            //MainClass.PrintMaze(maze, units);
-            return new List<Unit>();
-
-        }
-        static List<(int X, int Y)> PossibleMoves((int X, int Y) pos, List<string> maze, List<Unit> units)
-        {
-            var result = new List<(int X, int Y)>();
-            foreach (var delta in new List<(int X, int Y)> { (0, -1), (-1, 0), (1, 0), (0, 1) })
-            {
-                if (maze[pos.Y + delta.Y][pos.X + delta.X] == '.' && !units.Any(u => u.Pos.X == pos.X + delta.X && u.Pos.Y == pos.Y + delta.Y))
-                {
-                    result.Add((pos.X + delta.X, pos.Y + delta.Y));
-                }
+                case "addr":
+                    result[output] = inputRegisters[inputA] + inputRegisters[inputB];
+                    break;
+                case "addi":
+                    result[output] = inputRegisters[inputA] + inputB;
+                    break;
+                case "mulr":
+                    result[output] = inputRegisters[inputA] * inputRegisters[inputB];
+                    break;
+                case "muli":
+                    result[output] = inputRegisters[inputA] * inputB;
+                    break;
+                case "banr":
+                    result[output] = inputRegisters[inputA] & inputRegisters[inputB];
+                    break;
+                case "bani":
+                    result[output] = inputRegisters[inputA] & inputB;
+                    break;
+                case "borr":
+                    result[output] = inputRegisters[inputA] | inputRegisters[inputB];
+                    break;
+                case "bori":
+                    result[output] = inputRegisters[inputA] | inputB;
+                    break;
+                case "setr":
+                    result[output] = inputRegisters[inputA]; 
+                    break;
+                case "seti":
+                    result[output] = inputA;
+                    break;
+                case "gtir":
+                    result[output] = inputA > inputRegisters[inputB] ? 1 : 0;
+                    break;
+                case "gtri":
+                    result[output] = inputRegisters[inputA] > inputB ? 1 : 0;
+                    break;
+                case "gtrr":
+                    result[output] = inputRegisters[inputA] > inputRegisters[inputB] ? 1 : 0;
+                    break;
+                case "eqir":
+                    result[output] = inputA == inputRegisters[inputB] ? 1 : 0;
+                    break;
+                case "eqri":
+                    result[output] = inputRegisters[inputA] == inputB ? 1 : 0;
+                    break;
+                case "eqrr":
+                    result[output] = inputRegisters[inputA] == inputRegisters[inputB] ? 1 : 0;
+                    break;
             }
             return result;
         }
-        public List<Unit> FindMeleeUnits(List<Unit> units)
-        {
-            var closestUnits = units.Where(u => u.GetType() != this.GetType())
-                                    .Where(u => new List<(int X, int Y)> { (0, -1), (-1, 0), (1, 0), (0, 1) }
-                                                    .Any(delta => u.Pos.X == Pos.X + delta.X && u.Pos.Y == Pos.Y + delta.Y));
-            //if (closestUnits.Any()) Console.WriteLine($"The {this.GetType()} at {Pos} can directly attack {String.Join(",", closestUnits)}");
-            return closestUnits.ToList();
-        }
-        public override string ToString()
-        {
-            return $"{Symbol()}{Pos}";
-        }
     }
-    class Elf : Unit
-    {
-        public Elf(int X, int Y) : base(X, Y)
-        {
-        }
-        public Elf (Elf e) : base(e) { }
-        public override char Symbol() { return 'E'; }
-    }
-    class Goblin : Unit
-    {
-        public Goblin(int X, int Y) : base(X, Y)
-        {
 
-        }
-        public Goblin(Goblin g) : base(g) { }
-        public override char Symbol() { return 'G'; }
-    }
 
     class MainClass
     {
-        public static void PrintMaze(List<string> maze, List<Unit> units)
+        public static int TestOpcodes(int[] before, int[] after, int A, int B, int C)
         {
-            for (int i = 0; i < maze.Count; i++)
-            {
-                var line = new StringBuilder(maze[i]);
-                foreach (var unitInLine in units.Where(u => u.Pos.Y == i))
-                {
-                    line[unitInLine.Pos.X] = unitInLine.Symbol();
-                    line.Append($"  {unitInLine}:{unitInLine.hp}");
-                }
-                Console.WriteLine(line.ToString());
-            }
-            Console.WriteLine();
-        }
-        public static int RunCombat(List<string> maze, List <Unit> units, int elfAttackPower, bool acceptElfDeath)
-        {
-            var round = 1;
-            for (; ; round++)
-            {
-                foreach (var unit in units.OrderBy((u) => u.Pos.Y).ThenBy(u => u.Pos.X).ToList())
-                {
-                    if (unit.hp <= 0)
-                        continue;
-                    if (!units.Any(u => u.GetType() != unit.GetType())) goto combat_complete;
-                    var oldPosition = unit.Pos;
-                    var targets = unit.MoveAndFindTargets(maze, units);
+            string[] opCodes = { "addr", "addi", "mulr", "muli", "banr", "bani", "borr", "bori", "setr", "seti", "gtir", "gtri", "gtrr", "eqir", "eqri", "eqrr" };
 
-                    var meleeUnits = unit.FindMeleeUnits(units);
-                    if (meleeUnits.Any())
-                    {
+            var matchingOpcodes = 0;
+            foreach (var opCode in opCodes)
+            {
+                int[] beforeTest = new int[4];
 
-                        var unitToAttack = meleeUnits.OrderBy(u => u.hp).ThenBy(u => u.Pos.Y).ThenBy(u => u.Pos.X).First();
-                        if (unit is Goblin)
-                        {
-                            unitToAttack.hp -= 3;
-                            if (unitToAttack.hp <= 0 && !acceptElfDeath)
-                            {
-                                Console.WriteLine("An elf dies, we cannot have that!");
-                                return -1;    // No elf may be harmed!
-                            }
-                        }
-                        else
-                        {
-                            unitToAttack.hp -= elfAttackPower;
-                        }
-                        if (unitToAttack.hp <= 0)
-                        {
-                            units.Remove(unitToAttack);
-                        }
-                    }
+                before.CopyTo(beforeTest, 0);
+                var instr = new Instruction(opCode, A, B, C);
+                var afterExecuteTest = instr.Execute(beforeTest);
+                if (afterExecuteTest.SequenceEqual(after))
+                {
+                   // Console.WriteLine($"Found a match for {opCode}");
+                    matchingOpcodes++;
                 }
-                Console.WriteLine($"After {round} round(s):");
-                PrintMaze(maze, units);
             }
-        combat_complete:
-            round--;    // Last round was not complete
-            Console.WriteLine($"Combat ended after {round} rounds with attack power of {elfAttackPower}: ");
-            Console.WriteLine($"{units.Count} units remaining with a total of {units.Sum(u => u.hp)}");
-            Console.WriteLine($"Outcome: {units.Sum(u => u.hp) * round}");
-            return units.Sum(u => u.hp) * round;
+            return matchingOpcodes;
         }
         public static void Main(string[] args)
         {
             string[] input = System.IO.File.ReadAllLines("../../input.txt");
-            var maze = new List<string>(); // # = wall .=open
-            var initialUnits = new List<Unit>();
+            
+           // var registers = new int[4];
 
-            for (int i = 0; i < input.Length; i++)
+            // Test code:
+            var beforeTest = new int[] { 3, 2, 1, 1 };
+            var afterTest = new int[] { 3, 2, 2, 1 };
+
+            Console.WriteLine($"{TestOpcodes(beforeTest, afterTest, 2, 1, 2)} match the given behaviour in the test case.");
+
+            var inputEnum = input.GetEnumerator();
+            int matchThreeOrMore = 0;
+            while (inputEnum.MoveNext())
             {
-                var line = input[i];
-                int index = -1;
-                while ((index = line.IndexOf('G', index + 1)) != -1)
+                string line = inputEnum.Current as string;
+                if (!line.StartsWith("Before")) break;
+
+                var before = line.Split(" [,]".ToCharArray()).Where(x => Int32.TryParse(x, out int n)).Select(x => { if (Int32.TryParse(x, out int result)) return result; else return 0; }).ToArray();
+
+                inputEnum.MoveNext();
+                line = inputEnum.Current as string;
+                var instr = line.Split(" [,]".ToCharArray()).Select(x => Convert.ToInt32(x)).ToArray();
+
+                inputEnum.MoveNext();
+                line = inputEnum.Current as string;
+                var after = line.Split(" [,]".ToCharArray()).Where(x => Int32.TryParse(x, out int n)).Select(x => { if (Int32.TryParse(x, out int result)) return result; else return 0; }).ToArray();
+
+                if (TestOpcodes(before, after, instr[1], instr[2], instr[3]) >= 3 )
                 {
-                    var goblin = new Goblin(index, i);
-                    initialUnits.Add(goblin);
+                    Console.WriteLine($"Found an input that matches more than 3: {String.Join(" ", instr)}");
+                    matchThreeOrMore++;
                 }
-                while ((index = line.IndexOf('E', index + 1)) != -1)
-                {
-                    var elf = new Elf(index, i);
-                    initialUnits.Add(elf);
-                }
-                line = line.Replace('G', '.').Replace('E', '.');
-                maze.Add(line);
+                inputEnum.MoveNext();
             }
-
-            Console.WriteLine("Initial position:");
-            PrintMaze(maze, initialUnits);
-            var outcome = RunCombat(maze, initialUnits, 3, true);
-            Console.WriteLine($"With default power of 3 and accepting deaths, the outcome was {outcome}:");
-            PrintMaze(maze, initialUnits);
-
-
-            outcome = -1;
-            List<Unit> units = null; 
-            for (int power=4; outcome == -1; power++)
-            {
-                units = new List<Unit>();
-                for (int i = 0; i < input.Length; i++)
-                {
-                    var line = input[i];
-                    int index = -1;
-                    while ((index = line.IndexOf('G', index + 1)) != -1)
-                    {
-                        var goblin = new Goblin(index, i);
-                        units.Add(goblin);
-                    }
-                    while ((index = line.IndexOf('E', index + 1)) != -1)
-                    {
-                        var elf = new Elf(index, i);
-                        units.Add(elf);
-                    }
-                }
-                outcome = RunCombat(maze, units, power, false);
-            }
-            Console.WriteLine($"At lowest increased power, the outcome was {outcome}.");
-            PrintMaze(maze, units);
+            Console.WriteLine($"A total of {matchThreeOrMore} patterns match three or more opcodes");
             Console.ReadKey();
         }
     }
