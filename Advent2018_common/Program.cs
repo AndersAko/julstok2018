a8327a36763bf6e96e420debd9042eda1f2436c4
Anders Kökeritz <anders.kokeritz@gmail.com>
2018-12-27 22:48:02 +0100

    Day 23

--- Advent2018_common/Program.cs
+++ Advent2018_common/Program.cs
@@ -7,207 +7,169 @@ using System.Text;
 
 namespace Advent2018_common
 {
-    class Cave
+    class NanoBot
     {
-        Dictionary<(int, int), int> erosionLevel = new Dictionary<(int, int), int>();
-        int depth;
-        public (int X, int Y) target;
+        public (int X, int Y, int Z) pos;
+        public int range;
 
-        public Cave(int depth, (int X, int Y) target)
+        public List<NanoBot> OverlappingBots = new List<NanoBot>();
+
+        public NanoBot(int x, int y, int z, int range)
         {
-            this.depth = depth;
-            this.target = target;
+            pos.X = x;
+            pos.Y = y;
+            pos.Z = z;
+            this.range = range;
         }
-
-        public int ErosionLevel((int X, int Y) coord)
+        // Is the other bot in range of this?
+        public bool InRange((int X, int Y, int Z) otherPos)
         {
-            if (!erosionLevel.ContainsKey(coord))
-            {
-                if (coord == target)
-                {
-                    erosionLevel[coord] = 0 + depth;
-                }
-                else if (coord.Y == 0)
-                {
-                    erosionLevel[coord] = (coord.X * 16807 + depth) % 20183;
-                }
-                else if (coord.X == 0)
-                {
-                    erosionLevel[coord] = (coord.Y * 48271 + depth) % 20183;
-                }
-                else
-                {
-                    erosionLevel[coord] = (ErosionLevel((coord.X - 1, coord.Y)) * ErosionLevel((coord.X, coord.Y - 1)) + depth) % 20183;
-                }
-            }
-            return erosionLevel[coord];
+            var distance = Math.Abs(otherPos.X - pos.X) + Math.Abs(otherPos.Y - pos.Y) + Math.Abs(otherPos.Z - pos.Z);
+            return distance <= range;
         }
-        public List<CavePosition> PossibleMoves(CavePosition cpos)
+        // At least one square overlaps in range
+        public bool Overlap(NanoBot other)
         {
-            var result = new List<CavePosition>();
-
-            if (cpos.pos.X > 0) result.Add(new CavePosition(cpos.pos.X - 1, cpos.pos.Y, cpos.equiped, cpos));
-            if (cpos.pos.Y > 0) result.Add(new CavePosition(cpos.pos.X, cpos.pos.Y - 1, cpos.equiped, cpos));
-            result.Add(new CavePosition(cpos.pos.X + 1, cpos.pos.Y, cpos.equiped, cpos));
-            result.Add(new CavePosition(cpos.pos.X, cpos.pos.Y + 1, cpos.equiped, cpos));
-            if (cpos.equiped != Tool.climbing) result.Add(new CavePosition(cpos.pos.X, cpos.pos.Y, Tool.climbing, cpos));
-            if (cpos.equiped != Tool.torch) result.Add(new CavePosition(cpos.pos.X, cpos.pos.Y, Tool.torch, cpos));
-            if (cpos.equiped != Tool.neither) result.Add(new CavePosition(cpos.pos.X, cpos.pos.Y, Tool.neither, cpos));
-
-            var toReturn = result.Where(cp => cp.CompatibleTool(this)).ToList();
-            //Console.WriteLine($"From position {cpos}, the possible moves are: {string.Join(";", toReturn)}");
-            return toReturn;
+            var distance = Math.Abs(other.pos.X - pos.X) + Math.Abs(other.pos.Y - pos.Y) + Math.Abs(other.pos.Z - pos.Z);
+            return distance <= range + other.range;
         }
-    }
-    public enum Tool { climbing, torch, neither };
-    struct CavePosition
-    {
-        public (int X, int Y) pos;
-        public Tool equiped;
-        public int cost;
-        public List<string> path;   // CavePosition.ToString() list
-
-        public CavePosition(int X, int Y, Tool tool, CavePosition? previous = null)
+        private int rangeDist(int min, int max, int x)
         {
-            pos.X = X;
-            pos.Y = Y;
-            equiped = tool;
-            if (previous == null) path = new List<string>();
-            else
-            {
-                path = new List<string>(previous.Value.path)
-                {
-                    $"{pos.X},{pos.Y}:{equiped}"
-                };
-            }
-            if (previous == null) cost = 0;
-            else cost = previous.Value.cost + (previous.Value.equiped == tool ? 1 : 7);
+            if (x > max) return x - max;
+            if (x < min) return min - x;
+            return 0;
         }
-
-        public bool CompatibleTool(Cave cave)
+        public int DistanceToCube(SearchCube cube)
         {
-            var terrainType = cave.ErosionLevel(pos) % 3;
-            switch (terrainType)
-            {
-                case 0:
-                    return equiped == Tool.climbing || equiped == Tool.torch;
-                case 1:
-                    return equiped == Tool.climbing || equiped == Tool.neither;
-                case 2:
-                    return equiped == Tool.torch || equiped == Tool.neither;
-            }
-            return false;
+            return rangeDist(cube.min.X, cube.max.X, pos.X) +
+                   rangeDist(cube.min.Y, cube.max.Y, pos.Y) +
+                   rangeDist(cube.min.Z, cube.max.Z, pos.Z);
         }
         public override string ToString()
         {
-            return $"{pos.X},{pos.Y}:{equiped}";
+            return $"{pos}{range}";
         }
     }
-    class AStarSorting : IComparer<CavePosition>
+    struct SearchCube
     {
-        public Cave cave;
+        public (int X, int Y, int Z) min;
+        public (int X, int Y, int Z) max;
 
-        public AStarSorting(Cave cave)
+        List<NanoBot> bots;
+        int? _botsInRange;
+        public int BotsInRange
         {
-            this.cave = cave;
+            get
+            {
+                var thisCube = this;
+                if (_botsInRange == null) _botsInRange = bots.Count(bot => bot.DistanceToCube(thisCube) <= bot.range);
+                return _botsInRange.Value;
+            }
         }
-        public int Compare(CavePosition a, CavePosition b)
+        public int Size
         {
-            var estimateA = a.cost + Math.Abs(cave.target.X - a.pos.X) + Math.Abs(cave.target.Y - a.pos.Y);
-            var estimateB = b.cost + Math.Abs(cave.target.X - b.pos.X) + Math.Abs(cave.target.Y - b.pos.Y);
-            if (estimateA.CompareTo(estimateB) != 0) return estimateA.CompareTo(estimateB);
-            if (a.pos.CompareTo(b.pos) != 0) return a.pos.CompareTo(b.pos);
-            return a.equiped.CompareTo(b.equiped);
-//            return string.Compare(a.ToString(), b.ToString(), StringComparison.Ordinal);
+            get
+            {
+                return max.X - min.X + max.Y - min.Y + max.Z - min.Z;
+            }
+        }
+        public IEnumerable<SearchCube> SplitCube()
+        {
+            for (int x = min.X; x <= max.X; x += (max.X - min.X + 2) / 2)
+            {
+                for (int y = min.Y; y <= max.Y; y += (max.Y - min.Y + 2) / 2)
+                {
+                    for (int z = min.Z; z <= max.Z; z += (max.Z - min.Z + 2) / 2)
+                    {
+                        (int X, int Y, int Z) newCubeMax = (Math.Min(x + (max.X - min.X) / 2, max.X),
+                                                             Math.Min(y + (max.Y - min.Y) / 2, max.Y),
+                                                             Math.Min(z + (max.Z - min.Z) / 2, max.Z));
+                        if (newCubeMax.X < x || newCubeMax.Y < y || newCubeMax.Z < z) Console.WriteLine($"Split cube too small: {(x, y, z)}- {newCubeMax}"); 
+                        yield return new SearchCube((x, y, z), newCubeMax, bots);
+                    }
+                }
+            }
+        }
+
+        public SearchCube((int X, int Y, int Z) min, (int X, int Y, int Z) max, List<NanoBot> bots)
+        {
+            this.min = min;
+            this.max = max;
+            this.bots = bots;
+            this._botsInRange = null;
+        }
+        public override string ToString()
+        {
+            return $"{min} - {max}: {BotsInRange}";
+        }
+    }
+    class CubeSorter : IComparer<SearchCube>
+    {
+        List<NanoBot> bots;
+        public CubeSorter(List<NanoBot> bots)
+        {
+            this.bots = bots;
+        }
+        public int Compare(SearchCube a, SearchCube b)
+        {
+            // Most bots
+            if (a.BotsInRange != b.BotsInRange) return a.BotsInRange.CompareTo(b.BotsInRange);
+            // then closest to 0,0,0
+            if (a.min.X + a.min.Y + a.min.Z != b.min.X + b.min.Y + b.min.Z) return (a.min.X + a.min.Y + a.min.Z).CompareTo(b.min.X + b.min.Y + b.min.Z);
+            // then size
+            if (a.Size != b.Size) return a.Size.CompareTo(b.Size);
+            // then identity (essentially)
+            return a.min.CompareTo(b.min);
         }
     }
     class MainClass
     {
-        public static int ShortestPath(Cave cave)
-        {
-            var positionsToCheck = new SortedSet<CavePosition>(new AStarSorting(cave));
-
-            var startPosition = new CavePosition(0, 0, Tool.torch);
-            var checkedPositions = new HashSet<string>();       // CavePosition.ToString()
-            checkedPositions.Add(startPosition.ToString());
-
-            positionsToCheck.UnionWith(cave.PossibleMoves(startPosition));
-
-            while (positionsToCheck.Any())
-            {
-                var checkPosition = positionsToCheck.First();
-                positionsToCheck.Remove(checkPosition);
-                checkedPositions.Add(checkPosition.ToString());
-
-                if (checkPosition.pos == cave.target && checkPosition.equiped == Tool.torch)
-                {
-                    Console.WriteLine($"Found him at {checkPosition.pos} after {checkPosition.cost} minutes");
-                    Console.WriteLine($"Path: {String.Join(";", checkPosition.path)}");
-                    return checkPosition.cost;
-                }
-                var newPositions = cave.PossibleMoves(checkPosition)
-                                       .Where(cp => !checkedPositions.Contains(cp.ToString())).ToList();
-                //foreach (var cp in newPositions)
-                //{
-                //    var adding = positionsToCheck.Add(cp);
-                //    if (adding) Console.WriteLine($"Added: {cp}");
-                //    else Console.WriteLine($"Didn't add {cp}");
-                //}
-                positionsToCheck.UnionWith(newPositions);
-             }
-            return 0; // Didn't find a path...
-        }
-
         public static void Main(string[] args)
         {
-            // string[] input = System.IO.File.ReadAllLines("../../input.txt");
-            int depth;
-            (int X, int Y) target;
+            string[] input = System.IO.File.ReadAllLines("../../input.txt");
 
-            // Puzzle
-            depth = 3066;
-            target = (13, 726);
-            //depth = 510;
-            //target = (10, 10);
-            var typeMarker = ".=|";
-
-            var cave = new Cave(depth, target);
-
-            for (int y = 0; y < 16; y++)
+            var bots = new List<NanoBot>();
+            foreach (var line in input)
             {
-                for (int x = 0; x < 16; x++)
+                var parts = line.Split(" <>,=".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                                .Where(p => int.TryParse(p, out int result)).Select(p => Convert.ToInt32(p)).ToList();
+
+                bots.Add(new NanoBot(parts[0], parts[1], parts[2], parts[3]));
+            }
+            var botWithGreatestRange = bots.OrderByDescending(b => b.range).First();
+
+            Console.WriteLine($"Bot with greatest range: {botWithGreatestRange}");
+            var inRangeOfBot1 = bots.Where(b => botWithGreatestRange.InRange(b.pos)).ToList();
+
+            Console.WriteLine($"A total of {inRangeOfBot1.Count} bots are in range.");
+            var minExtent = (bots.Min(b => b.pos.X), bots.Min(b => b.pos.Y), bots.Min(b => b.pos.Z));
+            var maxExtent = (bots.Max(b => b.pos.X), bots.Max(b => b.pos.Y), bots.Max(b => b.pos.Z));
+
+            var toSearch = new SortedSet<SearchCube>(new CubeSorter(bots));
+            toSearch.Add(new SearchCube(minExtent, maxExtent, bots));
+
+            while (toSearch.Any())
+            {
+                var cube = toSearch.Last();
+                toSearch.Remove(cube);
+                   if (cube.Size == 0)
                 {
-                    if ((x, y) == target)
+                    Console.WriteLine($"Found a 1 sized box with {cube.BotsInRange} bots at {cube.min}: Distance: {cube.min.X + cube.min.Y + cube.min.Z}");
+                    break;
+                }
+                else
+                {
+                    // Split cube
+                    foreach (var newCube in cube.SplitCube())
                     {
-                        Console.Write("T");
-                    }
-                    else if ((x, y) == (0, 0))
-                    {
-                        Console.Write("M");
-                    }
-                    else
-                    {
-                        var erosion = (cave.ErosionLevel((x, y)));
-                        Console.Write(typeMarker[erosion % 3]);
+                        if (!toSearch.Add(newCube)) Console.WriteLine($"Did not add cube: {newCube.min}-{newCube.max}");
                     }
                 }
-                Console.WriteLine();
+
             }
 
-            int risklevel = 0;
-            for (int y = 0; y <= target.Y; y++)
-            {
-                for (int x = 0; x <= target.X; x++)
-                {
-                    risklevel += cave.ErosionLevel((x, y)) % 3;
-                }
-            }
-            Console.WriteLine($"Total risk level: {risklevel}");
 
-            // Find the shortest path
-            Console.WriteLine($"Finding shortest path: {ShortestPath(cave)}");
-
-
+            //            Console.WriteLine($"{maxBotsInRange} at {bestPositions.OrderByDescending(p => p.R).ThenBy(p => p.X + p.Y + p.Z).First()}");
             Console.ReadKey();
         }
     }
