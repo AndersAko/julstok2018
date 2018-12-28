ce6cb85ff7ec0c7b952392b46b9113bad3ec9694
Anders Kökeritz <anders.kokeritz@gmail.com>
2018-12-28 16:58:20 +0100

    Day 24

--- Advent2018_common/Program.cs
+++ Advent2018_common/Program.cs
@@ -1,176 +1,280 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-
+using System.Text.RegularExpressions;
 using System.Linq;
 using System.Text;
 
 namespace Advent2018_common
 {
-    class NanoBot
+    public class ArmyGroup
     {
-        public (int X, int Y, int Z) pos;
-        public int range;
+        public int Number;
+        public int HitPoints;
+        public int AttackDamage;
+        public string AttackType;
+        public int Initiative;
+        public bool Selected = false;
 
-        public List<NanoBot> OverlappingBots = new List<NanoBot>();
+        public List<string> Weaknesses = new List<string>();
+        public List<string> Immunities = new List<string>();
 
-        public NanoBot(int x, int y, int z, int range)
-        {
-            pos.X = x;
-            pos.Y = y;
-            pos.Z = z;
-            this.range = range;
-        }
-        // Is the other bot in range of this?
-        public bool InRange((int X, int Y, int Z) otherPos)
-        {
-            var distance = Math.Abs(otherPos.X - pos.X) + Math.Abs(otherPos.Y - pos.Y) + Math.Abs(otherPos.Z - pos.Z);
-            return distance <= range;
-        }
-        // At least one square overlaps in range
-        public bool Overlap(NanoBot other)
-        {
-            var distance = Math.Abs(other.pos.X - pos.X) + Math.Abs(other.pos.Y - pos.Y) + Math.Abs(other.pos.Z - pos.Z);
-            return distance <= range + other.range;
-        }
-        private int rangeDist(int min, int max, int x)
-        {
-            if (x > max) return x - max;
-            if (x < min) return min - x;
-            return 0;
-        }
-        public int DistanceToCube(SearchCube cube)
-        {
-            return rangeDist(cube.min.X, cube.max.X, pos.X) +
-                   rangeDist(cube.min.Y, cube.max.Y, pos.Y) +
-                   rangeDist(cube.min.Z, cube.max.Z, pos.Z);
-        }
-        public override string ToString()
-        {
-            return $"{pos}{range}";
-        }
-    }
-    struct SearchCube
-    {
-        public (int X, int Y, int Z) min;
-        public (int X, int Y, int Z) max;
-
-        List<NanoBot> bots;
-        int? _botsInRange;
-        public int BotsInRange
+        public ArmyGroup Target = null;
+        public virtual Int64 Power
         {
             get
             {
-                var thisCube = this;
-                if (_botsInRange == null) _botsInRange = bots.Count(bot => bot.DistanceToCube(thisCube) <= bot.range);
-                return _botsInRange.Value;
+                return Number * AttackDamage;
             }
         }
-        public int Size
+        public ArmyGroup(int num, int hp, int dmg, string type, int initiative, string weaknesses)
         {
-            get
+            Number = num;
+            HitPoints = hp;
+            AttackDamage = dmg;
+            AttackType = type;
+            Initiative = initiative;
+
+            foreach (var weakness in weaknesses.TrimStart("( ".ToCharArray()).Split(";".ToCharArray(), StringSplitOptions.RemoveEmptyEntries))
             {
-                return max.X - min.X + max.Y - min.Y + max.Z - min.Z;
-            }
-        }
-        public IEnumerable<SearchCube> SplitCube()
-        {
-            for (int x = min.X; x <= max.X; x += (max.X - min.X + 2) / 2)
-            {
-                for (int y = min.Y; y <= max.Y; y += (max.Y - min.Y + 2) / 2)
+                var weakTypes = weakness.Split(" ,)".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+                if (weakTypes[0] == "immune")
                 {
-                    for (int z = min.Z; z <= max.Z; z += (max.Z - min.Z + 2) / 2)
+                    for (int i = 2; i < weakTypes.Count(); i++)
                     {
-                        (int X, int Y, int Z) newCubeMax = (Math.Min(x + (max.X - min.X) / 2, max.X),
-                                                             Math.Min(y + (max.Y - min.Y) / 2, max.Y),
-                                                             Math.Min(z + (max.Z - min.Z) / 2, max.Z));
-                        if (newCubeMax.X < x || newCubeMax.Y < y || newCubeMax.Z < z) Console.WriteLine($"Split cube too small: {(x, y, z)}- {newCubeMax}"); 
-                        yield return new SearchCube((x, y, z), newCubeMax, bots);
+                        Immunities.Add(weakTypes[i]);
+                    }
+                }
+                else if (weakTypes[0] == "weak")
+                {
+                    for (int i = 2; i < weakTypes.Count(); i++)
+                    {
+                        Weaknesses.Add(weakTypes[i]);
                     }
                 }
             }
         }
-
-        public SearchCube((int X, int Y, int Z) min, (int X, int Y, int Z) max, List<NanoBot> bots)
+        public ArmyGroup(ArmyGroup fromCopy)
         {
-            this.min = min;
-            this.max = max;
-            this.bots = bots;
-            this._botsInRange = null;
+            Number = fromCopy.Number;
+            HitPoints = fromCopy.HitPoints;
+            AttackDamage = fromCopy.AttackDamage;
+            AttackType = fromCopy.AttackType;
+            Initiative = fromCopy.Initiative;
+            Weaknesses = new List<string>(fromCopy.Weaknesses);
+            Immunities = new List<string>(fromCopy.Immunities);
+        }
+        public ArmyGroup Clone()
+        {
+            return (ArmyGroup)this.MemberwiseClone();
+        }
+        // Amount of damage dealt to target
+        public virtual Int64 Damage(ArmyGroup target)
+        {
+            if (target.Immunities.Contains(AttackType)) return 0;
+            if (target.Weaknesses.Contains(AttackType)) return Power * 2;
+            else return Power;
         }
         public override string ToString()
         {
-            return $"{min} - {max}: {BotsInRange}";
+            return $"{Number} {HitPoints}hp Attack:{AttackDamage} {AttackType} Initiative:{Initiative} Weakness:{String.Join(",", Weaknesses)} Immune:{String.Join(",", Immunities)}";
         }
     }
-    class CubeSorter : IComparer<SearchCube>
+    public class ImmuneSystem : ArmyGroup
     {
-        List<NanoBot> bots;
-        public CubeSorter(List<NanoBot> bots)
+        public ImmuneSystem(int num, int hp, int dmg, string type, int initiative, string weaknesses) : base(num, hp, dmg, type, initiative, weaknesses)
         {
-            this.bots = bots;
+
         }
-        public int Compare(SearchCube a, SearchCube b)
+        public ImmuneSystem(ImmuneSystem immune) : base(immune) { }
+
+        public override Int64 Damage(ArmyGroup target)
         {
-            // Most bots
-            if (a.BotsInRange != b.BotsInRange) return a.BotsInRange.CompareTo(b.BotsInRange);
-            // then closest to 0,0,0
-            if (a.min.X + a.min.Y + a.min.Z != b.min.X + b.min.Y + b.min.Z) return (a.min.X + a.min.Y + a.min.Z).CompareTo(b.min.X + b.min.Y + b.min.Z);
-            // then size
-            if (a.Size != b.Size) return a.Size.CompareTo(b.Size);
-            // then identity (essentially)
-            return a.min.CompareTo(b.min);
+            if (target.Immunities.Contains(AttackType)) return 0;
+            if (target.Weaknesses.Contains(AttackType)) return Power * 2;
+            else return Power;
+        }
+        public override Int64 Power
+        {
+            get
+            {
+                return Number * (AttackDamage + MainClass.Boost);
+            }
         }
     }
+    public class Infection : ArmyGroup
+    {
+        public Infection(int num, int hp, int dmg, string type, int initiative, string weaknesses) : base(num, hp, dmg, type, initiative, weaknesses)
+        {
+
+        }
+        public Infection(Infection infection) : base(infection) { }
+    }
+    public struct BoostLevel
+    {
+        public int level;
+        public bool highEnough;
+        public BoostLevel(int l, bool e)
+        {
+            level = l;
+            highEnough = e;
+        }
+    }
+
+    /*
+     * Immune System:
+       17 units each with 5390 hit points (weak to radiation, bludgeoning) with an attack that does 4507 fire damage at initiative 2
+       989 units each with 1274 hit points (immune to fire; weak to bludgeoning, slashing) with an attack that does 25 slashing damage at initiative 3
+
+       Infection:
+       801 units each with 4706 hit points (weak to radiation) with an attack that does 116 bludgeoning damage at initiative 1
+       4485 units each with 2961 hit points (immune to radiation; weak to fire, cold) with an attack that does 12 slashing damage at initiative 4
+    */
+
+
     class MainClass
     {
+        //static Regex groupDescriptionRx = new Regex(@"(?<num>\d+) units each with (?<hp>\d+) hit points (?<weakness>\((immune|weak) to[ ;,\w]+\))? with an attack that does (?<dmg>\d+) (?<type>\w) damage at initiative (?<init>\d+)");
+        static Regex groupDescriptionRx = new Regex(@"(?<num>\d+) units each with (?<hp>\d+) hit points(?<weakness> \((immune|weak) to[ ;,\w]+\))? with an attack that does (?<dmg>\d+) (?<type>\w*) damage at initiative (?<init>\d+)");
+        public static int Boost = 0;
+
         public static void Main(string[] args)
         {
             string[] input = System.IO.File.ReadAllLines("../../input.txt");
 
-            var bots = new List<NanoBot>();
+            bool infection = false;
+            var OriginalReindeerSystem = new List<ArmyGroup>();
+
             foreach (var line in input)
             {
-                var parts = line.Split(" <>,=".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
-                                .Where(p => int.TryParse(p, out int result)).Select(p => Convert.ToInt32(p)).ToList();
-
-                bots.Add(new NanoBot(parts[0], parts[1], parts[2], parts[3]));
-            }
-            var botWithGreatestRange = bots.OrderByDescending(b => b.range).First();
-
-            Console.WriteLine($"Bot with greatest range: {botWithGreatestRange}");
-            var inRangeOfBot1 = bots.Where(b => botWithGreatestRange.InRange(b.pos)).ToList();
-
-            Console.WriteLine($"A total of {inRangeOfBot1.Count} bots are in range.");
-            var minExtent = (bots.Min(b => b.pos.X), bots.Min(b => b.pos.Y), bots.Min(b => b.pos.Z));
-            var maxExtent = (bots.Max(b => b.pos.X), bots.Max(b => b.pos.Y), bots.Max(b => b.pos.Z));
-
-            var toSearch = new SortedSet<SearchCube>(new CubeSorter(bots));
-            toSearch.Add(new SearchCube(minExtent, maxExtent, bots));
-
-            while (toSearch.Any())
-            {
-                var cube = toSearch.Last();
-                toSearch.Remove(cube);
-                   if (cube.Size == 0)
+                if (line.StartsWith("Immune", StringComparison.Ordinal)) continue;
+                if (line.StartsWith("Infection", StringComparison.Ordinal))
                 {
-                    Console.WriteLine($"Found a 1 sized box with {cube.BotsInRange} bots at {cube.min}: Distance: {cube.min.X + cube.min.Y + cube.min.Z}");
-                    break;
+                    infection = true;
+                    continue;
                 }
-                else
+                Match match = groupDescriptionRx.Match(line);
+                if (!match.Success) continue;
+                ArmyGroup army;
+                if (infection) army = new Infection(Convert.ToInt32(match.Groups["num"].ToString()), Convert.ToInt32(match.Groups["hp"].ToString()), Convert.ToInt32(match.Groups["dmg"].ToString()),
+                                         match.Groups["type"].ToString(), Convert.ToInt32(match.Groups["init"].ToString()), match.Groups["weakness"].ToString());
+                else army = new ImmuneSystem(Convert.ToInt32(match.Groups["num"].ToString()), Convert.ToInt32(match.Groups["hp"].ToString()), Convert.ToInt32(match.Groups["dmg"].ToString()),
+                         match.Groups["type"].ToString(), Convert.ToInt32(match.Groups["init"].ToString()), match.Groups["weakness"].ToString());
+                OriginalReindeerSystem.Add(army);
+
+                Console.WriteLine($"Added: {army}");
+            }
+
+            List<ArmyGroup> ReindeerSystem;
+            Boost = 0;
+            ReindeerSystem = RunCombat(OriginalReindeerSystem);
+
+            BoostLevel LowBoost = new BoostLevel(0, false);
+
+            Boost = 10000;
+            ReindeerSystem = RunCombat(OriginalReindeerSystem);
+            BoostLevel HighBoost = new BoostLevel(10000, !ReindeerSystem.Any(a => a is Infection));
+
+            do
+            {
+                ReindeerSystem = new List<ArmyGroup>(OriginalReindeerSystem.Select(a => a.Clone()));
+                if (!HighBoost.highEnough)
                 {
-                    // Split cube
-                    foreach (var newCube in cube.SplitCube())
+                    HighBoost.level += HighBoost.level - LowBoost.level;
+                    Boost = HighBoost.level;
+                    ReindeerSystem = RunCombat(ReindeerSystem);
+                    HighBoost.highEnough = !ReindeerSystem.Any(a => a is Infection);
+                }
+                else if (LowBoost.highEnough)
+                {
+                    LowBoost.level -= HighBoost.level - LowBoost.level;
+                    Boost = LowBoost.level;
+                    ReindeerSystem = RunCombat(ReindeerSystem);
+                    LowBoost.highEnough = !ReindeerSystem.Any(a => a is Infection);
+                }
+                else if (!LowBoost.highEnough && HighBoost.highEnough)
+                {
+                    Boost = HighBoost.level - (HighBoost.level - LowBoost.level) / 2;
+                    ReindeerSystem = RunCombat(ReindeerSystem);
+                    if (!ReindeerSystem.Any(a => a is Infection))
                     {
-                        if (!toSearch.Add(newCube)) Console.WriteLine($"Did not add cube: {newCube.min}-{newCube.max}");
+                        HighBoost.highEnough = true;
+                        HighBoost.level = Boost;
+                    }
+                    else
+                    {
+                        LowBoost.highEnough = false;
+                        LowBoost.level = Boost;
+                    }
+                }
+                else Console.WriteLine($"Strange: {LowBoost}, {HighBoost}");
+                Console.WriteLine($"Searching... {LowBoost.level} {LowBoost.highEnough} to {HighBoost.level} {HighBoost.highEnough}");
+            } while (HighBoost.level - LowBoost.level > 1);
+
+            Console.WriteLine($"Probably lowest boost value: {HighBoost.level}");
+            for (int i=HighBoost.level -10 ; i < HighBoost.level+5; i++)
+            {
+                Boost = i;
+                ReindeerSystem = RunCombat(OriginalReindeerSystem);
+            }
+
+            Console.ReadKey();
+        }
+
+        private static List<ArmyGroup> RunCombat(List<ArmyGroup> OriginalReindeerSystem)
+        {
+            var ReindeerSystem = new List<ArmyGroup>(OriginalReindeerSystem.Select(a => a.Clone()));
+            int TotalUnits = ReindeerSystem.Sum(a => a.Number);
+
+            int Round = 1;
+            while (ReindeerSystem.Any(a => a is ImmuneSystem) && ReindeerSystem.Any(a => a is Infection))
+            {
+                //Console.WriteLine($"Round {Round++}");
+                //Console.WriteLine("Immune System:");
+                //foreach (var immune in ReindeerSystem.Where(a => a is ImmuneSystem))
+                //{
+                //    Console.WriteLine($"{immune}");
+                //}
+                //Console.WriteLine("Infection:");
+                //foreach (var infections in ReindeerSystem.Where(a => a is Infection))
+                //{
+                //    Console.WriteLine($"{infections}");
+                //}
+                //Console.WriteLine();
+
+                // Target selection
+                foreach (var group in ReindeerSystem.OrderByDescending(g => g.Power).ThenByDescending(g => g.Initiative))
+                {
+                    var target = ReindeerSystem.Where(a => a.GetType() != group.GetType() && !a.Selected).OrderByDescending(a => group.Damage(a))
+                                .ThenByDescending(a => a.Power).ThenByDescending(a => a.Initiative).FirstOrDefault();
+                    if (target != null && group.Damage(target) == 0) target = null;
+                    group.Target = target;
+                    if (target != null) target.Selected = true;
+                }
+
+                // Atack phase
+                foreach (var group in ReindeerSystem.OrderByDescending(g => g.Initiative))
+                {
+                    if (group.Target != null && group.Number >0)
+                    {
+                        //Console.WriteLine($"{group} attacked {group.Target}");
+                        //Console.WriteLine($"  dealing {group.Damage(group.Target)} killing {group.Damage(group.Target) / group.Target.HitPoints} units of {group.Target.Number}.");
+                        group.Target.Number -= (int)(group.Damage(group.Target) / (long)group.Target.HitPoints);
                     }
                 }
 
+                ReindeerSystem = ReindeerSystem.Where(a => a.Number > 0).ToList();
+                foreach (var a in ReindeerSystem)
+                {
+                    a.Selected = false;
+                }
+                if (TotalUnits == ReindeerSystem.Sum(a => a.Number)) break;
+                TotalUnits = ReindeerSystem.Sum(a => a.Number);
             }
-
-
-            //            Console.WriteLine($"{maxBotsInRange} at {bestPositions.OrderByDescending(p => p.R).ThenBy(p => p.X + p.Y + p.Z).First()}");
-            Console.ReadKey();
+            Console.WriteLine($"{Boost} - {ReindeerSystem.Count()} groups of {String.Join("; ",ReindeerSystem.GroupBy(a=>a.GetType()).Select(g => $"{g.First().GetType()} {g.Sum(a => a.Number)}"))} " +
+            	$"remain with a total of {ReindeerSystem.Sum(a => a.Number)} units");
+            return ReindeerSystem;
         }
     }
 }
